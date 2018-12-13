b24fb496c1944caccc9555781a72e06f86750948
Zev Spilman <zevspilman@yahoo.com>
2018-12-13 03:41:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function dwarfRollCall(dwarves) {
+  for (let i = 0; i < dwarves.length; i ++){
+    dwarves.push(`${i + 1}${dwarves[i]}`)
+    if (i < dwarves.length -1){
+      dwarves.push(',')
+    }
+  }
 }
 
 function summonCaptainPlanet(planeteerCalls){
