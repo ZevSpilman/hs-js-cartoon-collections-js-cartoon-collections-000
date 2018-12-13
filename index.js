843a621f8c4e9cc4bf1c1b3538f06c540da0adee
Zev Spilman <zevspilman@yahoo.com>
2018-12-13 15:51:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function dwarfRollCall(dwarves) {
+  for (let i = 0; i < dwarves.length; i ++){
+    dwarves.push(`${i + 1}${dwarves[i]}`)
+    if (i < dwarves.length -1){
+      dwarves.push(',')
+    }
+  }
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var newPlaneteerCalls = []
+  for (let i = 0; i < planeteerCalls.length; i ++){
+newPlaneteerCalls.push(`${planeteerCalls[i]}!`)
+}
+return newPlaneteerCalls.toUpperCase( )
 }
 
 function longPlaneteerCalls(words) {
 }
 
 function findTheCheese (foods) {
-}
+  var cheese = ["cheddar", "gouda", "camembert"]
+  for (let i = 0; i < foods.length; i ++){
+    if (cheese.includes(foods[i]) === true){
+      return foods[i]
+    }
+  }
+     return 'no cheese!'
+    }
+
