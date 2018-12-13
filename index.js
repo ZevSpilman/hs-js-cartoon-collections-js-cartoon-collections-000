e427cad03dc274f9621c5310fd8e37172918fd93
Zev Spilman <zevspilman@yahoo.com>
2018-12-13 06:03:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 function dwarfRollCall(dwarves) {
+  for (let i = 0; i < dwarves.length; i ++){
+    dwarves.push(`${i + 1}${dwarves[i]}`)
+    if (i < dwarves.length -1){
+      dwarves.push(',')
+    }
+  }
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls){\
+
 }
 
 function longPlaneteerCalls(words) {
 }
 
 function findTheCheese (foods) {
-}
+  var cheese = ["cheddar", "gouda", "camembert"]
+  for (let i = 0; i < foods.length; i ++){
+    if (cheese.includes(foods[i]) === true){
+      return foods[i]
+    }
+  }
+     return 'no cheese!'
+    }
+
